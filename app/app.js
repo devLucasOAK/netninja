24a4e1b165bd53355106968f5a5cc388900b3309
devLucasOAK <devlucasoak@outook.com>
2021-03-04 01:32:36 -0300

    ngRoute

--- app/app.js
+++ app/app.js
@@ -1,4 +1,22 @@
-var myNinjaApp = angular.module('myNinjaApp', []);
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+
+    $routeProvider
+    .when('/home', {
+        templateUrl: 'views/home.html'
+    })
+    .when('/directory', {
+        templateUrl: 'views/directory.html',
+        controller: 'NinjaController'
+    })
+    .otherwise({
+        redirectTo: '/home'
+    })
+}]);
+
+
+
 
 myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 
@@ -58,10 +76,6 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
     ];
 
 }]);
-
-myNinjaApp.config(function(){
-
-});
 
 myNinjaApp.run(function(){
 
